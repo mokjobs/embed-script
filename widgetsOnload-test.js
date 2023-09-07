d9f6488673ee3fbaae0a706d3febf82af4d4a743
mokjobs <seongmok.kim@teamblind.com>
2023-09-07 12:46:50 +0900

    modify logics

--- widgetsOnload-test.js
+++ widgetsOnload-test.js
@@ -4,49 +4,56 @@
   var script = document.currentScript;
   var alias = script.dataset.embed_id;
   var blockquoteId = "blind-embed-".concat(alias);
-  var loadingBlockquoteId = "blind-embed-".concat(alias, "-loading-").concat(Date.now());
+  var loadingBlockquoteId = "blind-embed-"
+    .concat(alias, "-loading-")
+    .concat(Date.now());
   var framePreload = function framePreload(url) {
     var frameObject = document.createElement("iframe");
     frameObject.src = url;
-    frameObject.height = "0"; // height set to 0  
+    frameObject.height = "0"; // height set to 0
     frameObject.width = "100%";
-    frameObject.style = "display:block; max-width: 512px;border:none; overflow:hidden; margin: 0 auto;";
+    frameObject.style =
+      "display:block; max-width: 512px;border:none; overflow:hidden; margin: 0 auto;";
     frameObject.scrolling = "no";
     frameObject.loading = "lazy";
     frameObject.frameborder = "0";
     frameObject.allowfullscreen = "true";
-    frameObject.allow = "autoplay; clipboard-write; encrypted-media; picture-in-picture;";
+    frameObject.allow =
+      "autoplay; clipboard-write; encrypted-media; picture-in-picture;";
     frameObject.style.visibility = "hidden"; // set to hidden to actually load the image
     return frameObject;
   };
   var handleLoad = function handleLoad(iframe) {
-    console.log('iframe loaded')
-    window.addEventListener("message", function (e) {
+    console.log("iframe loaded, set the height");
+    var loading = document.getElementById(loadingBlockquoteId);
+    console.log("loading", loading);
+    iframe.height = '100%';
+    iframe.style.visibility = "initial";
 
-      console.log('current iframe', iframe)
+    if (loading && loading.parentNode) {
+      loading.parentNode.removeChild(loading); // remove the blockquote object
+    }
 
-      var message = e.data;
-      console.log("message sent", message)
-      if (e.source == iframe.contentWindow && message.height) {
+    window.addEventListener(
+      "message",
+      function (e) {
+        console.log("current iframe", iframe);
 
-        iframe.height = message.height;
-      } else {
-        iframe.height="100%";
-        iframe.style.visibility = "initial";
-      }
-      iframe.style.visibility = "initial"; // show the iframe
-      var loading = document.getElementById(loadingBlockquoteId);
-      console.log('loading', loading)
-      if (loading && loading.parentNode) {
-        loading.parentNode.removeChild(loading); // remove the blockquote object
-      }
-    }, false);
+        var message = e.data;
+        console.log("message sent", message);
+        if (e.source == iframe.contentWindow && message.height) {
+          iframe.height = message.height;
+        }
+        iframe.style.visibility = "initial"; // show the iframe
+      },
+      false
+    );
   };
   var blockquote = document.getElementById(blockquoteId);
-  console.log('the blockquote', blockquote)
+  console.log("the blockquote", blockquote);
   blockquote.id = loadingBlockquoteId;
   var genIframe = framePreload(blockquote.dataset.blindPermalink);
-  console.log('iframe', genIframe)
+  console.log("iframe", genIframe);
   genIframe.onload = function () {
     handleLoad(genIframe, blockquote);
   };
